916691e82e59ae2b2f8a09076ca85b6b888d9dd7
[renu-mahato1980] <renukamahato034@gmail.com>
2026-06-10 00:25:34 +0530

    test

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,15 @@
+// module.exports = {
+//   url: "mongodb://localhost:27017/jsr",
+// };
+
+
 module.exports = {
-  url: "mongodb://localhost:27017/jsr",
+  url: "mongodb+srv://renukamahato_dbuser:<db_password>@cluster0.1gorory.mongodb.net/",
 };
+
+
+
+
+
+
+
